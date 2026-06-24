b9969b77bb6a0ad4904c0893fad071ceeb99a091
Abhisumat Kashyap <abhisumatkashyap@gmail.com>
2026-06-24 17:12:13 +0530

    chore: drift database class - empty schema stub

--- lib/core/database/app_database.dart
+++ lib/core/database/app_database.dart
@@ -1,1 +1,23 @@
-// Stub - will be implemented in a later phase
+import 'dart:io';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+
+part 'app_database.g.dart';
+
+@DriftDatabase(tables: [], daos: [])
+class AppDatabase extends _$AppDatabase {
+  AppDatabase() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'meditrack.sqlite'));
+    return NativeDatabase.createInBackground(file);
+  });
+}
