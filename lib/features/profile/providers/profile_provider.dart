b9ae743017d103dadf92fbd15e4a4ff769bcbc38
Abhisumat Kashyap <abhisumatkashyap@gmail.com>
2026-06-24 18:37:57 +0530

    feat: profile riverpod provider

--- lib/features/profile/providers/profile_provider.dart
+++ lib/features/profile/providers/profile_provider.dart
@@ -1,1 +1,34 @@
-// Stub - will be implemented in a later phase
+import 'package:drift/drift.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../../core/database/app_database.dart';
+import '../daos/profile_dao.dart';
+
+part 'profile_provider.g.dart';
+
+@riverpod
+ProfileDao profileDao(ProfileDaoRef ref) {
+  return ref.read(appDatabaseProvider).profileDao;
+}
+
+@riverpod
+class ProfileNotifier extends _$ProfileNotifier {
+  @override
+  Future<UserProfile?> build() async {
+    final dao = ref.read(profileDaoProvider);
+    return dao.getProfile();
+  }
+
+  Future<void> saveProfile(UserProfilesCompanion profile) async {
+    final dao = ref.read(profileDaoProvider);
+    final existing = await dao.getProfile();
+    if (existing == null) {
+      await dao.insertProfile(profile);
+    } else {
+      await dao.updateProfile(
+        profile.copyWith(id: Value(existing.id)),
+      );
+    }
+    ref.invalidateSelf();
+  }
+}
---
+++ lib/features/profile/providers/profile_provider.g.dart
@@ -0,0 +1,43 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'profile_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$profileDaoHash() => r'9fe53ed8ec9b354be31dfa9301ee634fd8ed09bd';
+
+/// See also [profileDao].
+@ProviderFor(profileDao)
+final profileDaoProvider = AutoDisposeProvider<ProfileDao>.internal(
+  profileDao,
+  name: r'profileDaoProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$profileDaoHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ProfileDaoRef = AutoDisposeProviderRef<ProfileDao>;
+String _$profileNotifierHash() => r'227d369a256d91df2fd2002948a74eb67580fbcb';
+
+/// See also [ProfileNotifier].
+@ProviderFor(ProfileNotifier)
+final profileNotifierProvider =
+    AutoDisposeAsyncNotifierProvider<ProfileNotifier, UserProfile?>.internal(
+      ProfileNotifier.new,
+      name: r'profileNotifierProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$profileNotifierHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$ProfileNotifier = AutoDisposeAsyncNotifier<UserProfile?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
