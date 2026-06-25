4f94c23d87fa85741b596cba06755a9c49221915
Abhisumat Kashyap <abhisumatkashyap@gmail.com>
2026-06-25 22:31:17 +0530

    feat: doctor visit DAO - insert, list, upcoming follow-up query

--- lib/features/doctor_visits/daos/doctor_visit_dao.dart
+++ lib/features/doctor_visits/daos/doctor_visit_dao.dart
@@ -6,25 +6,29 @@ import '../models/doctor_visit.dart';
 part 'doctor_visit_dao.g.dart';
 
 @DriftAccessor(tables: [DoctorVisits])
-class DoctorVisitDao extends DatabaseAccessor<AppDatabase> with _$DoctorVisitDaoMixin {
+class DoctorVisitDao extends DatabaseAccessor<AppDatabase>
+    with _$DoctorVisitDaoMixin {
   DoctorVisitDao(super.db);
 
   Future<int> insertVisit(DoctorVisitsCompanion entry) =>
       into(doctorVisits).insert(entry);
 
   Future<List<DoctorVisit>> getAllVisits() =>
-      (select(doctorVisits)..orderBy([(t) => OrderingTerm.desc(t.visitDate)])).get();
+      (select(doctorVisits)
+            ..orderBy([(t) => OrderingTerm.desc(t.visitDate)]))
+          .get();
 
   Stream<List<DoctorVisit>> watchAllVisits() =>
-      (select(doctorVisits)..orderBy([(t) => OrderingTerm.desc(t.visitDate)])).watch();
+      (select(doctorVisits)
+            ..orderBy([(t) => OrderingTerm.desc(t.visitDate)]))
+          .watch();
 
-  Future<DoctorVisit?> getVisit(int id) =>
-      (select(doctorVisits)..where((t) => t.id.equals(id))).getSingleOrNull();
-
-  Future<int> updateVisit(DoctorVisitsCompanion entry) =>
-      (update(doctorVisits)..where((t) => t.id.equals(entry.id.value)))
-          .write(entry);
-
-  Future<int> deleteVisit(int id) =>
-      (delete(doctorVisits)..where((t) => t.id.equals(id))).go();
+  Future<DoctorVisit?> getNextUpcomingFollowUp() async {
+    final now = DateTime.now();
+    return (select(doctorVisits)
+          ..where((t) => t.followUpDate.isBiggerOrEqualValue(now))
+          ..orderBy([(t) => OrderingTerm.asc(t.followUpDate)])
+          ..limit(1))
+        .getSingleOrNull();
+  }
 }
