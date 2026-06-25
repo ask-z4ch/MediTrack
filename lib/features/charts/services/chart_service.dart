7d4ee219fc188a8e1aa94474b4266942e9959d0c
Abhisumat Kashyap <abhisumatkashyap@gmail.com>
2026-06-25 15:27:25 +0530

    feat: ChartService wrapper around fl_chart for reusable config

--- lib/features/charts/services/chart_service.dart
+++ lib/features/charts/services/chart_service.dart
@@ -1,1 +1,44 @@
-// Stub - will be implemented in a later phase
+import 'package:fl_chart/fl_chart.dart';
+import 'package:intl/intl.dart';
+
+import '../../../core/constants/app_colors.dart';
+import '../../../core/constants/health_thresholds.dart';
+
+class ChartService {
+  static ExtraLinesData bpRangeBands() {
+    return ExtraLinesData(horizontalLines: [
+      HorizontalLine(
+        y: BPThreshold.normalSystolicMax.toDouble(),
+        color: AppColors.normal.withOpacity(0.25),
+        strokeWidth: 1,
+        dashArray: [5, 5],
+        label: HorizontalLineLabel(
+          show: true,
+          labelResolver: (_) => 'Normal',
+          style: const TextStyle(color: AppColors.normal, fontSize: 10),
+        ),
+      ),
+      HorizontalLine(
+        y: BPThreshold.borderlineSystolicMax.toDouble(),
+        color: AppColors.borderline.withOpacity(0.25),
+        strokeWidth: 1,
+        dashArray: [5, 5],
+      ),
+    ]);
+  }
+
+  static SideTitles dateSideTitles(List<DateTime> dates) {
+    return SideTitles(
+      showTitles: true,
+      interval: 1,
+      getTitlesWidget: (value, meta) {
+        final i = value.toInt();
+        if (i < 0 || i >= dates.length) return const SizedBox();
+        return Text(
+          DateFormat('dd/MM').format(dates[i]),
+          style: const TextStyle(fontSize: 9),
+        );
+      },
+    );
+  }
+}
